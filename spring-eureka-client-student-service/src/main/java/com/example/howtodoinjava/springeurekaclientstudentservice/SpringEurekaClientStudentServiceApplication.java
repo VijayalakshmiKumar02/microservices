af3b4c8025335e50b825dd779dfb60bb11aaf0f3
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2019-03-19 13:01:01 +0530

    Update SpringEurekaClientStudentServiceApplication.java

--- spring-eureka-client-student-service/src/main/java/com/example/howtodoinjava/springeurekaclientstudentservice/SpringEurekaClientStudentServiceApplication.java
+++ spring-eureka-client-student-service/src/main/java/com/example/howtodoinjava/springeurekaclientstudentservice/SpringEurekaClientStudentServiceApplication.java
@@ -3,9 +3,9 @@ package com.example.howtodoinjava.springeurekaclientstudentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SpringEurekaClientStudentServiceApplication {
 
 	public static void main(String[] args) {

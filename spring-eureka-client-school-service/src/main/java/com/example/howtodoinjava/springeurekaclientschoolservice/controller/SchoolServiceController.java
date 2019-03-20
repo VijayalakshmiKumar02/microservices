277f282bbe78a8e1ba5c232302111038f1ed9403
VijayalakshmiKumar02 <>
2019-03-20 10:51:46 +0530

    Delegate added

--- spring-eureka-client-school-service/src/main/java/com/example/howtodoinjava/springeurekaclientschoolservice/controller/SchoolServiceController.java
+++ spring-eureka-client-school-service/src/main/java/com/example/howtodoinjava/springeurekaclientschoolservice/controller/SchoolServiceController.java
@@ -10,14 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import java.util.Date;
+import com.example.howtodoinjava.spring.delegate.StudentServiceDelegate;
 @RestController
 public class SchoolServiceController {
+	//@Autowired
+	//RestTemplate restTemplate;
 	@Autowired
-	RestTemplate restTemplate;
-
+    StudentServiceDelegate studentServiceDelegate;
 	@RequestMapping(value = "/getSchoolDetails/{schoolname}", method = RequestMethod.GET)
+	
 	public String getStudents(@PathVariable String schoolname) {
+        System.out.println("Going to call student service to get data!");
+        return studentServiceDelegate.callStudentServiceAndGetData(schoolname);
+    }
+	
+	
+	
+	
+}
+	
+	
+	/*public String getStudents(@PathVariable String schoolname) {
 		System.out.println("Getting School details for " + schoolname);
 		String response = restTemplate.exchange("http://student-service/getStudentDetailsForSchool/{schoolname}", HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
 		}, schoolname).getBody();
@@ -26,10 +41,16 @@ public class SchoolServiceController {
 
 		return "School Name -  " + schoolname + " \n Student Details " + response;
 	}
+	public String callStudentServiceAndGetData_Fallback(String schoolname) {
+		 
+        System.out.println("Student Service is down!!! fallback route enabled...");
+ 
+        return "CIRCUIT BREAKER ENABLED!!! No Response From Student Service at this moment. " +
+                    " Service will be back shortly - " + new Date();
+    }
 
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
-	}
-}
+	}*/

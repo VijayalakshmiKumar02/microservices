33a085970720b80ad1f06261d6264ce7260244ca
VijayalakshmiKumar02 <>
2019-03-20 11:37:59 +0530

    school service

--- spring-eureka-client-school-service/src/main/java/com/example/howtodoinjava/springeurekaclientschoolservice/controller/SchoolServiceController.java
+++ spring-eureka-client-school-service/src/main/java/com/example/howtodoinjava/springeurekaclientschoolservice/controller/SchoolServiceController.java
@@ -1,57 +1,24 @@
 package com.example.howtodoinjava.springeurekaclientschoolservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.example.howtodoinjava.springeurekaclientschoolservice.delegate.StudentServiceDelegate;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import java.util.Date;
+
 @RestController
 public class SchoolServiceController {
-	//@Autowired
-	//RestTemplate restTemplate;
+	
 	@Autowired
-    StudentServiceDelegate studentServiceDelegate;
+	StudentServiceDelegate studentServiceDelegate;
+
 	@RequestMapping(value = "/getSchoolDetails/{schoolname}", method = RequestMethod.GET)
-	
 	public String getStudents(@PathVariable String schoolname) {
-        System.out.println("Going to call student service to get data!");
-        return studentServiceDelegate.callStudentServiceAndGetData(schoolname);
-    }
-	
-	
-	
+		System.out.println("Going to call student service to get data!");
+		return studentServiceDelegate.callStudentServiceAndGetData(schoolname);
+	}
 	
 }
 	
-	
-	/*public String getStudents(@PathVariable String schoolname) {
-		System.out.println("Getting School details for " + schoolname);
-		String response = restTemplate.exchange("http://student-service/getStudentDetailsForSchool/{schoolname}", HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
-		}, schoolname).getBody();
-
-		System.out.println("Response Received as " + response);
-
-		return "School Name -  " + schoolname + " \n Student Details " + response;
-	}
-	public String callStudentServiceAndGetData_Fallback(String schoolname) {
-		 
-        System.out.println("Student Service is down!!! fallback route enabled...");
- 
-        return "CIRCUIT BREAKER ENABLED!!! No Response From Student Service at this moment. " +
-                    " Service will be back shortly - " + new Date();
-    }
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}*/

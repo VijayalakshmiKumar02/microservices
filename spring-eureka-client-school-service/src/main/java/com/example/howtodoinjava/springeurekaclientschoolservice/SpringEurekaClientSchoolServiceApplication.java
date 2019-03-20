aa0b2762f4cc80992f89cbb17a09b40bc9903bdd
VijayalakshmiKumar02 <>
2019-03-20 10:36:40 +0530

    Hystrix enabled

--- spring-eureka-client-school-service/src/main/java/com/example/howtodoinjava/springeurekaclientschoolservice/SpringEurekaClientSchoolServiceApplication.java
+++ spring-eureka-client-school-service/src/main/java/com/example/howtodoinjava/springeurekaclientschoolservice/SpringEurekaClientSchoolServiceApplication.java
@@ -3,9 +3,15 @@ package com.example.howtodoinjava.springeurekaclientschoolservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class SpringEurekaClientSchoolServiceApplication {
 
 	public static void main(String[] args) {
